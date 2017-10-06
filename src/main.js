8258ee1035768cabf40a2ac5779436139afff515
Edward Guo <edward.h.guo@gmail.com>
2017-10-05 23:33:11 -0700

    Render the board with tiles

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-Vue.component('board', {
-  props: ['hello'],
+Vue.component('tile', {
+  props: ['data'],
   template: `
-    <div class="board">
-      {{ hello }}
+    <div class="tile">
+      {{ data.mark }}
     </div>
   `
 });
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    tiles: [
+      { id: 0, mark: '' },
+      { id: 1, mark: '' },
+      { id: 2, mark: '' },
+      { id: 3, mark: '' },
+      { id: 4, mark: '' },
+      { id: 5, mark: '' },
+      { id: 6, mark: '' },
+      { id: 7, mark: '' },
+      { id: 8, mark: '' }
+    ]
+  }
 });
