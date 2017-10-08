1cd9529011227b55c4aa54c55b1d17c207c4a7c6
Edward Guo <edward.h.guo@gmail.com>
2017-10-07 21:43:20 -0700

    Minor refactoring, use shorthand

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 Vue.component('tile', {
-  props: ['data'],
+  props: ['mark'],
   template: `
     <a class="tile">
-      {{ data.mark }}
+      {{ mark }}
     </a>
   `
 });
@@ -11,20 +11,16 @@ new Vue({
   el: '#app',
   data: {
     tiles: [
-      { id: 0, mark: '' },
-      { id: 1, mark: '' },
-      { id: 2, mark: '' },
-      { id: 3, mark: '' },
-      { id: 4, mark: '' },
-      { id: 5, mark: '' },
-      { id: 6, mark: '' },
-      { id: 7, mark: '' },
-      { id: 8, mark: '' }
+      '-', '-', '-',
+      '-', '-', '-',
+      '-', '-', '-'
     ]
   },
   methods: {
     markTile: function (event) {
-      console.log(event.target);
+      const id = event.target.id;
+      console.log(id);
+      this.tiles[id] = 'X';
     }
   }
 });
