6e3bb0ae9013b6d9555f7de29efcb79f6a133012
Edward Guo <edward.h.guo@gmail.com>
2017-10-09 22:52:43 -0700

    New game functionality

--- src/main.js
+++ src/main.js
@@ -7,26 +7,38 @@ Vue.component('tile', {
   `
 });
 
+const initialState = () => ({
+  currentPlayer: 'X',
+  tiles: [
+    { id: 0, mark: '-' },
+    { id: 1, mark: '-' },
+    { id: 2, mark: '-' },
+    { id: 3, mark: '-' },
+    { id: 4, mark: '-' },
+    { id: 5, mark: '-' },
+    { id: 6, mark: '-' },
+    { id: 7, mark: '-' },
+    { id: 8, mark: '-' },
+  ]
+});
+
+function markTile(event) {
+  const id = event.target.id;
+  if (this.tiles[id].mark === '-') {
+    this.tiles[id].mark = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  }
+}
+
+function newGame() {
+  Object.assign(this.$data, initialState());
+}
+
 new Vue({
   el: '#app',
-  data: {
-    tiles: [
-      { id: 0, mark: '-' },
-      { id: 1, mark: '-' },
-      { id: 2, mark: '-' },
-      { id: 3, mark: '-' },
-      { id: 4, mark: '-' },
-      { id: 5, mark: '-' },
-      { id: 6, mark: '-' },
-      { id: 7, mark: '-' },
-      { id: 8, mark: '-' },
-    ]
-  },
+  data: initialState(),
   methods: {
-    markTile: function (event) {
-      const id = event.target.id;
-      console.log(id);
-      this.tiles[id].mark = 'X';
-    }
+    markTile,
+    newGame
   }
 });
