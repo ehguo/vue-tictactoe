22e052e3f3d32e0968cf27854f781ac142bf632a
Edward Guo <edward.h.guo@gmail.com>
2017-10-09 22:31:57 -0700

    Working data bind

--- src/main.js
+++ src/main.js
@@ -11,16 +11,22 @@ new Vue({
   el: '#app',
   data: {
     tiles: [
-      '-', '-', '-',
-      '-', '-', '-',
-      '-', '-', '-'
+      { id: 0, mark: '-' },
+      { id: 1, mark: '-' },
+      { id: 2, mark: '-' },
+      { id: 3, mark: '-' },
+      { id: 4, mark: '-' },
+      { id: 5, mark: '-' },
+      { id: 6, mark: '-' },
+      { id: 7, mark: '-' },
+      { id: 8, mark: '-' },
     ]
   },
   methods: {
     markTile: function (event) {
       const id = event.target.id;
       console.log(id);
-      this.tiles[id] = 'X';
+      this.tiles[id].mark = 'X';
     }
   }
 });
